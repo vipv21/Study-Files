01ddcd53ed27dfbe6e6157e18447bffa6709639d
vipv21 <xiaoxingit@foxmail.com>
2018-06-01 20:06:47 +0800

    service端服务

--- Vueshop/service/index.js
+++ Vueshop/service/index.js
@@ -1,8 +1,8 @@
-const Koa =require('koa');  //引入koa
+const Koa = require('koa'); //声明常量 引入koa
 const app = new Koa();      //实例化 Koa
 
-app.use(async(ctx)=>{
-    ctx.body=" <h1>111111</h1>"
+app.use(async(ctx)=>{       
+    ctx.body = " <h1>111111</h1>"   //页面输出内容
 })
 
 app.listen(3000,()=>{       //监听3000端口
