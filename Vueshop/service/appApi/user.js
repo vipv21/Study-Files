927289c3d18c09fe42f2260eb1e94bfc6fa4d1a7
vipv21 <xiaoxingit@foxmail.com>
2018-07-11 21:44:09 +0800

    登录接口信息处理及状态返回

--- Vueshop/service/appApi/user.js
+++ Vueshop/service/appApi/user.js
@@ -22,10 +22,37 @@ router.post('/register', async (ctx) => {
             message:error
         }
     })
-
-
-
 })
+
+//用户登录接口
+router.post('/login', async (ctx) => {
+    let loginUser =ctx.request.body;    //获取登录信息
+    console.log(loginUser);
+    let userName = loginUser.userName;
+    let password = loginUser.password;
+    //引入User中的model
+    const User = mongoose.model('User');
+
+    await User.findOne({userName:userName}).exec()
+    .then(async(result)=>{
+        console.log(result);
+        if (result) {   //判断用户是否存在 开始对比密码
+            console.log(result);
+            let newUser =new User();    //因为是实例方法 必须要new出对象 才能调用
+            await newUser.comparePassword(password,result.password)
+            .then(isMatch =>{
+                ctx.body={ code:200 ,message:isMatch};
+            }).catch(error=>{
+                ctx.body={ code:500, message:error};
+            })
+        } else {
+            ctx.body={code:200 , message:'用户名不存在'};
+        }
+    }).catch(error=>{
+        ctx.body={code:500, message:error };
+    })
+})
+
 
 //暴露接口
 module.exports= router ;
