2fded942f2c58d5963955812e4a2f493e64da348
vipv21 <xiaoxingit@foxmail.com>
2018-07-15 20:33:05 +0800

    新增商品子类 api接口 并插入数据

--- Vueshop/service/appApi/goods.js
+++ Vueshop/service/appApi/goods.js
@@ -5,6 +5,8 @@ let router = new Router();
 
 const mongoose =require('mongoose');
 const fs = require('fs');
+//-------------------接口路由-------------------
+
 //插入所有商品数据 访问接口 --> /insertAllGoodsInfo
 router.get('/insertAllGoodsInfo',async(ctx)=>{
     fs.readFile('./data_json/newGoods.json','utf8',(err,data)=>{
@@ -43,6 +45,26 @@ router.get('/insertAllCategory',async(ctx)=>{   //异步的方法
     })
     ctx.body='开始导入数据...'
 })
+
+//插入 商品分类子类  接口 api
+router.get('/insertAllCategorySub', async (ctx) => { //异步的方法
+  fs.readFile('./data_json/categorySub.json', 'utf8', (err, data) => { //读取本地json数据
+    data = JSON.parse(data); //json对象
+    let saveCount = 0;
+    const CategorySub = mongoose.model('CategorySub'); //引入Catrgory
+    data.RECORDS.map((value, index) => { //循环保存数据到数据库
+      let newCategorySub = new CategorySub(value);
+      newCategorySub.save().then(() => {
+        saveCount++;
+        console.log('插入成功');
+      }).catch(error => {
+        console.log('插入失败');
+      })
+    })
+  })
+  ctx.body = '开始导入数据...'
+})
+
 
 
 module.exports =router ; //暴露路由接口出去
