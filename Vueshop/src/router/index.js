12ef6c93c79b39f653ae95e34823b950fb186300
vipv21 <xiaoxingit@foxmail.com>
2018-07-16 20:36:12 +0800

    新增商品详情路由接口

--- Vueshop/src/router/index.js
+++ Vueshop/src/router/index.js
@@ -4,7 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import ShoppingMall from '@/components/pages/ShoppingMall'
 import Register from '@/components/pages/Register'
 import Login from '@/components/pages/Login'
-
+import Goods from '@/components/pages/Goods'
 
 Vue.use(Router);
 
@@ -12,15 +12,13 @@ Vue.use(Router);
 export default new Router({
   routes: [
     //首页路由
-    {
-      path: '/',
-      name: 'ShoppingMall',
-      component: ShoppingMall
-    },
+    {path: '/', name: 'ShoppingMall', component: ShoppingMall},
     //注册路由
     {path:'/register' ,name:'Register' ,component:Register },
     //登录路由
     { path: '/login', name: 'Login', component: Login },
+    //商品详情路由
+    { path: '/goods', name: 'Goods', component: Goods },
   ]
 })
 
