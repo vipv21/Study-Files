f522baa2b9066d46365de7d2b17da14eee3c5814
vipv21 <xiaoxingit@foxmail.com>
2018-07-18 20:29:37 +0800

    新增商品详情模板页的 名称、价格、详情、菜单信息

--- Vueshop/src/components/pages/Goods.vue
+++ Vueshop/src/components/pages/Goods.vue
@@ -10,7 +10,31 @@
             />
         </div>
         <div class="topimage-div">
-            <img :src="goodsInfo.IMAGE1" width='100%'   alt="">
+            <img :src="goodsInfo.IMAGE1" width='100%' alt="">
+        </div>
+        <!-- 名称 -->
+        <div class="goods-name">{{goodsInfo.NAME}}</div>
+        <!-- 价格 -->
+        <div class="goods-price">价格： ￥{{goodsInfo.PRESENT_PRICE | moneyFilter}}元 </div>    
+        <!-- tab 选项卡 -->
+        <div>
+            <van-tabs>
+                <van-tab title="商品详情">
+                    <div class="detail" v-html="goodsInfo.DETAIL"></div>
+                </van-tab>
+                <van-tab title="评论">
+                    评论
+                </van-tab>
+            </van-tabs>
+        </div>
+        <!-- 底部菜单按钮 -->
+        <div class="goods-bottom">
+            <div>
+                <van-button size='large' type='primary'>加入购物车</van-button>
+            </div>
+            <div>
+                <van-button size='large' type='danger'>直接购买</van-button>
+            </div>
         </div>
     </div>
 </template>
@@ -19,12 +43,18 @@
     import axios from 'axios';
     import url from '@/serviceAPI.config.js';
     import {Toast} from 'vant' ;     //提示
+    import {toMoney} from '@/components/filter/moneyFilter.js'
 
     export default {
         data() {
             return {
                 goodsId: '775e575ce28a4f89b1dfe2c99eb08ae7',//模拟传递一个商品id
                 goodsInfo:{},   //商品详细信息
+            }
+        },
+        filters:{
+            moneyFilter(money){
+                return toMoney(money);
             }
         },
         created() {
@@ -64,5 +94,24 @@
 </script>
 
 <style scoped>
-
+    .goods-name,.goods-price{
+        background-color: #fff;
+    }
+    .detail{       /*  解决图片之间的间隙问题 */
+        font-size: 0;
+    }
+    .goods-bottom{
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        background-color: #ffffff;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        flex-flow: nowrap;
+    }
+    .goods-bottom >div{
+        flex: 1;
+        padding: 5px;
+    }
 </style>
