36bfc94e344a2b58ed35d2e68f36ef4089db0708
vipv21 <xiaoxingit@foxmail.com>
2018-08-01 21:19:22 +0800

    完善 上拉下拉数据的展示（类别和商品暂时不对，后期待更新）

--- Vueshop/src/components/pages/CategoryList.vue
+++ Vueshop/src/components/pages/CategoryList.vue
@@ -21,7 +21,7 @@
                 </van-col>
                 <van-col span='18'>
                     <div class="tabCategorySub">
-                        <van-tabs v-model="active">
+                        <van-tabs v-model="active" @click="onClickCategorySub">
                             <van-tab v-for="(item,index) in categorySub" :key='index' :title="item.MALL_SUB_NAME"></van-tab>
                         </van-tabs>
                     </div>
@@ -31,13 +31,17 @@
                         <van-pull-refresh v-model="isRefresh" @refresh='onRefresh' >
                             <!-- 上拉加载数据 -->
                             <van-list v-model="loading" :finished='finished' @load="onLoad" >
-                                <div class="list-item" v-for="item in list" :key='item'>
-                                    {{item}}
+                                <div class="list-item" v-for="(item,index) in goodList" :key='index'>
+                                    <div class="list-item-img">
+                                        <img :src="item.IMAGE1" width="100%" alt="">
+                                    </div>
+                                    <div class="list-item-text">
+                                        <div>{{item.NAME}}</div>
+                                        <div>￥{{item.ORI_PRICE}}元</div>
+                                    </div>
                                 </div>
                             </van-list>
                         </van-pull-refresh>
-
-
                     </div>
 
                 </van-col>
@@ -60,8 +64,12 @@
                 active :0 ,     //第0个开始激活的标签
                 loading : false ,   //加载状态 是否使用
                 finished: false , //上拉加载是否有数据
-                list :[] ,  //商品数据
-                isRefresh: false //下拉刷新 开关
+                goodList :[] ,      //商品数据
+                isRefresh: false, //下拉刷新 开关
+
+                page:1,             //商品列表页
+                categorySubId:'',   //商品子类id
+
             }
         },
         created() { //执行axios
@@ -83,7 +91,7 @@
                     method:'get'
                 })
                 .then(response=>{
-                    console.log(response)
+                    // console.log(response)
                     // 判断请求状态为200并且message不为空
                     if (response.data.code ==200 && response.data.message) {
                         this.category = response.data.message ;
@@ -99,6 +107,11 @@
             },
             clickCategry(index,categoryId){    //点击的索引值传入实现效果
                 this.categoryIndex = index ;
+                //初始化操作
+                this.page = 1;
+                this.finished =false ;
+                this.goodList =[] ;
+
                 this.getCategorySubByCategoryID(categoryId);
             },
             //根据大类获取小类
@@ -114,6 +127,9 @@
                     if (response.data.code == 200 && response.data.message) {
                         this.categorySub = response.data.message ;
                         this.active = 0 ;       //解决切换时数据不同步
+                        //读取大类信息
+                        this.categorySubId =this.categorySub[0].ID;
+                        this.onLoad();
                     }
                 }).catch(error=>{
                     console.log(error);
@@ -122,15 +138,10 @@
             //上拉加载
             onLoad(){
                 setTimeout(() => {
-                    for (let i = 0; i < 10; i++) {
-                        this.list.push(this.list.length+1)
-                        
-                    }
-                    this.loading =false ;
-                    if (this.list.length>=40) {
-                        this.finished = true;
-                    }
-                }, 500);
+                //如果有小类id则赋值 否则从子类的第一个中取id赋值
+                    this.categorySubId = this.getCategorySubId ? this.categorySubId:this.categorySub[0].ID
+                    this.getGoodList(); //获取商品列表信息
+                }, 1000);
             },
             //下拉刷新
             onRefresh(){
@@ -138,10 +149,46 @@
                 setTimeout(() => {
                     this.isRefresh =false ;
                     this.finished =false ;  //解决加载完后 上拉卡住
-                    this.list= [];  //list赋值为空
+                    this.goodList= [];  //goodList赋值为空
+                    this.page = 1 ; //页数
                     this.onLoad();  //重新执行加载
                 }, 500);   
-            }
+            },
+            //获取商品信息
+            getGoodList(){
+                axios({
+                    url:url.getGoodsListByCategorySubID,
+                    method:'post',
+                    data:{
+                        categorySubId:this.categorySubId ,
+                        page: this.page ,   //显示第几页
+                    }
+                }).then(response=>{
+                    console.log(response);
+                    if (response.data.code==200 &&response.data.message.length) {
+                        this.page++;
+                        //合并数组信息 旧的 + 新的
+                        this.goodList = this.goodList.concat(response.data.message);
+                    } else {
+                        //禁止上拉加载
+                        this.finished = true ;
+                    }
+                    this.loading = false ; 
+                }).catch(error=>{
+                    console.log(error);
+                })
+            },
+            //index索引值 title标题
+            onClickCategorySub(index,title){
+                //获取小类id值
+                this.categorySubId = this.categorySub[index].ID;
+                console.log('category:'+this.catergorySubId);
+                //切换小类类别 初始化值
+                this.goodList= [];  //清空值
+                this.finished = false;  //加载到底为false
+                this.page = 1;  //初始化为第一页
+                this.onLoad() ; //调用加载方法  
+            },
         }
     }
 </script>
@@ -159,12 +206,22 @@
         background-color: #ffffff;
     }
     .list-item{
-        text-align: center;
-        line-height: 80px;
+        display: flex;
+        flex-direction: row;
+        font-size:0.8rem;
         border-bottom: 1px solid #f0f0f0;
-        background-color: #ffffff;
+        background-color: #fff;
+        padding:5px;
     }
-    #list-div {
+    #list-div{
         overflow: scroll;
+    }
+    .list-item-img{
+        flex:8;
+    }
+    .list-item-text{
+        flex:16;
+        margin-top:10px;
+        margin-left:10px;
     }
 </style>
