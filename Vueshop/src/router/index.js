7e4b59b770b9b9feaa6fed9f200cc62d0c6a8a79
vipv21 <xiaoxingit@foxmail.com>
2018-08-12 20:06:40 +0800

    新增个人中心路由，底部菜单路由更新

--- Vueshop/src/router/index.js
+++ Vueshop/src/router/index.js
@@ -7,24 +7,34 @@ import Login from '@/components/pages/Login'
 import Goods from '@/components/pages/Goods'
 import CategoryList from '@/components/pages/CategoryList'
 import Cart from '@/components/pages/Cart'
+import Main from '@/components/pages/Main'
+import Member from '@/components/pages/Member'
 
 Vue.use(Router);
 
 
 export default new Router({
   routes: [
-    //首页路由
-    {path: '/', name: 'ShoppingMall', component: ShoppingMall},
+    //首页 底部tabbar
+    {path:'/main',component:Main,
+        children:[
+            //首页路由
+            {path: '/', name: 'ShoppingMall', component: ShoppingMall},
+            //商品类别路由
+            { path: '/CategoryList', name: 'CategoryList', component: CategoryList },
+            //购物车路由
+            { path: '/Cart', name: 'Cart', component: Cart },
+            //个人中心
+            {path:'/Member',name:'Member',component:Member},
+        ]
+    },
     //注册路由
     {path:'/register' ,name:'Register' ,component:Register },
     //登录路由
     { path: '/login', name: 'Login', component: Login },
     //商品详情路由
     { path: '/goods', name: 'Goods', component: Goods },
-    //商品类别路由
-    { path: '/CategoryList', name: 'CategoryList', component: CategoryList },
-    //购物车路由
-    { path: '/Cart', name: 'Cart', component: Cart },
+
   ]
 })
 
