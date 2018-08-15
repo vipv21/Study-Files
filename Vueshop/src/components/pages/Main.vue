27f197fefd7dd62dfc15a53b6d112b11f77ba1c8
vipv21 <xiaoxingit@foxmail.com>
2018-08-15 20:00:02 +0800

    新增缓存效果，减少请求

--- Vueshop/src/components/pages/Main.vue
+++ Vueshop/src/components/pages/Main.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <div class="main-div">
-            <router-view></router-view>
+            <!-- 实现缓存 -->
+            <keep-alive>
+                <router-view></router-view>
+            </keep-alive>
         </div>
         <van-tabbar v-model="active" @change="changeTabbar(active)">
             <van-tabbar-item icon='shop' >首页</van-tabbar-item>
